85033589062328680cf0516e77485844a5d5887d
rail888 <belokana@hanmail.net>
2025-06-04 14:33:24 +0900

    Initial commit

---
+++ src/main/java/pack/Sample01Application.java
@@ -0,0 +1,20 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Sample01Application {
+	@GetMapping("/")
+	public String hello(){
+		return "hello world!!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sample01Application.class, args);
+	}
+
+}
